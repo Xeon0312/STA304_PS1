d8c5692eef707b02ff41de577e2a2561c0185b52
Xeon0312 <caoboyu0312@gmail.com>
2020-09-25 17:31:38 +0800

    first commit

---
+++ Untitled.R
@@ -0,0 +1,15 @@
+library(blogdown)
+blogdown::new_site(
+  dir = "./web",
+  install_hugo = TRUE,
+  format = "toml",
+  sample = TRUE,
+  theme = "yihui/hugo-lithium",
+  hostname = "github.com",
+  theme_example = TRUE,
+  empty_dirs = FALSE,
+  to_yaml = TRUE,
+  serve = interactive()
+)
+
+
